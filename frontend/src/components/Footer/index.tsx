3d9edf4c3ffe66046c18f1509f38dda2d095616e
Silva <jrjrsilva@gmail.com>
2021-05-03 16:05:46 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const  Footer = () =>{
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/jrjrsilva" target="_blank" rel="noreferrer">José Raimundo Silva Santos</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/jr_.santts" target="_blank" rel="noreferrer">@jr_.santts</a></small></p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
